eab9e2e19719f8edea791a1716ed5d67260a1792
Satyanarayan Sen <60086933+Satyanarayan4434@users.noreply.github.com>
2023-06-15 11:24:11 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main class="flex justify-center items-center gap-4 flex-col min-h-screen">
+      <h1>Hello React</h1>
+    </main>
+  );
+}
+
+export default App;
